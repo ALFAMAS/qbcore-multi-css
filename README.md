6416432a5c4664247609ce7763aa074193ad1cff
ALFAMAS <arafat0951@gmail.com>
2022-01-31 00:25:38 +0600

    Removed Spawn and Added New Interior

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# [QBCore] [Multicharacter](https://github.com/qbcore-framework/qb-multicharacter) & Spawn Reworked CSS
+# [QBCore] [Multicharacter](https://github.com/qbcore-framework/qb-multicharacter) Reworked CSS
 # For Latest QBCore Public Server
 
-### Preview : https://youtu.be/GG_9eDq1u1E
+### Preview New Interior : https://youtu.be/
+### Preview Old Interior : https://youtu.be/GG_9eDq1u1E
 
 ### My Discord Server - [DISCORD](https://discord.gg/jSDMuNjpuw)
 
 ### SETUP 
 - Add these 2 lines your to `Server.cfg`.
 - Ensure qb-multicharacter 
-- Ensure qb-spawn
 - Done!
---
+++ qb-multicharacter/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
+
+Config.Interior = vector3(-1453.56, -551.53, 72.84) -- Interior to load where characters are previewed
+Config.PedCoords = vector4(-1453.56, -551.53, 72.84, 109.66) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-1453.58, -556.30, 72.88, 299.5) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-1458.66, -550.97, 73.00, 266.5) -- Camera coordinates for character preview screen
+Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
+
+--oldConfig.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
+--oldConfig.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
+--oldConfig.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
+--oldConfig.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
